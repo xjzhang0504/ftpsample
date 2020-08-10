9c993b242b61e928cb1dfa8144ffad03cb971954
xjzhang <xjzhang0504@163.com>
2020-08-10 23:44:02 +0800

    init

---
+++ src/main/java/com/xingyun/utils/SmartSftpUtils.java
@@ -0,0 +1,64 @@
+package com.xingyun.utils;
+
+import com.jcraft.jsch.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @sees http://www.jcraft.com/jsch/
+ * @author qing-fegn.zhao
+ */
+@Slf4j
+public final class SmartSftpUtils {
+	private static Logger log = LogManager.getLogger(SmartSftpUtils.class);
+
+	public static void main(String[] args) {
+
+		SmartSftpUtils.downloadFileBySftp("192.168.123.101" ,"charles","suhao-11","sshdata/test.txt","./");
+	}
+
+
+	/**
+	 *
+	 * @param remoteServerIp
+	 * @param username
+	 * @param password
+	 * @param remoteFilePath
+	 * @param localFilePath
+	 */
+
+
+	public static void downloadFileBySftp(String remoteServerIp,
+										  String username,
+										  String password,
+										  String remoteFilePath,
+										  String localFilePath){
+
+
+		JSch jsch = new JSch();
+		try {
+			Session session = jsch.getSession(username, remoteServerIp, 22);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword(password);
+			session.connect();
+
+			Channel channel = session.openChannel("sftp");
+			channel.connect();
+			ChannelSftp sftpChannel = (ChannelSftp) channel;
+			sftpChannel.get(remoteFilePath, localFilePath);
+			sftpChannel.exit();
+			session.disconnect();
+		} catch (JSchException e) {
+			//To change body of catch statement use File | Settings | File Templates.
+			log.error(e.getMessage(),e);
+		} catch (SftpException e) {
+			log.error(e.getMessage(),e);
+		}
+	}
+
+	/**
+	 * 禁用构造方法
+	 */
+	private SmartSftpUtils(){}
+}
